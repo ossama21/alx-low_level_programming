768dec296a2b823bcd8a851f8f00060e6cb5ae17
Oussama <ossamahattan@gmail.com>
2023-10-10 13:58:11 -0700

    all

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,36 @@
+#include "dog.h"
+
+/**
+ * new_dog - creates a new dog.
+ * @name: The dogs name
+ * @age: The dogs age
+ * @owner: The owner
+ * Return: The dogs
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *newDog;
+
+	if (name == NULL || owner == NULL)
+		return (NULL);
+
+	newDog = malloc(sizeof(dog_t));
+
+	if (newDog == NULL)
+		return (NULL);
+
+	newDog->name = strdup(name);
+	newDog->owner = strdup(owner);
+
+	if (newDog->name == NULL || newDog->owner == NULL)
+	{
+		free(newDog->name);
+		free(newDog->owner);
+		free(newDog);
+		return (NULL);
+	}
+
+	newDog->age = age;
+	return (newDog);
+}
