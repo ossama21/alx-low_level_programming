46e4cb439d320d008757c085be694f967233952a
Oussama <ossamahattan@gmail.com>
2023-10-12 15:08:12 -0700

    all

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings -  prints srings,folowed by a new line.
+ * @separator: ...
+ * @n:the number of the arguments
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+	char *str;
+
+	va_start(args, n);
+	if (separator == NULL)
+		separator = "";
+
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(args, char *);
+
+		if (str == NULL)
+			str = "(nil)";
+
+		printf("%s", str);
+
+		if (i < n - 1)
+			printf("%s", separator);
+	}
+	putchar('\n');
+	va_end(args);
+}
