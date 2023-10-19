5c13e11a131d28d15c4e9d0cc7bd223570882252
ossama21 <ossamahattan@gmail@com>
2023-10-19 13:51:52 -0700

    all

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,13 @@
-extern	printf		; the C function, to be called
 section .data
-	hello db "Hello, Holberton",0	; Define the string to be printed
-	format db "%s",10,0		; Define the format string with a newline character
+	format db "Hello, Holberton", 10, 0
 
 section .text
-	global main		; Entry point for the program
-	extern printf		; Declare the printf function from the C library
+	global main
+	extern printf
 
 main:
-	push rbp	;Save the base pointer
-	mov rdi, format	; Load the address of the format string into rdi
-	mov rsi, hello	; Load the address of the hello string into rsi
-	call printf	; Call the printf function to print the formatted string
-	pop rbp		; Restore the base pointer
-	ret		; Return from the main function
+	push rdi
+	mov rdi, format
+	call printf
+	pop rdi
+	ret
