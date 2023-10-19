0d41334017c68922711976d2e9b3b33a7c91f80a
ossama21 <ossamahattan@gmail@com>
2023-10-19 13:12:29 -0700

    all

--- 0x12-singly_linked_lists/100-main.c
+++ 0x12-singly_linked_lists/100-main.c
@@ -13,6 +13,6 @@ void print_mess(void) __attribute__((constructor));
  */
 int main(void)
 {
-    printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
-    return (0);
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
