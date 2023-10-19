0539f631f3f69a61de844cb464a8b20be0fcf3dd
ossama21 <ossamahattan@gmail@com>
2023-10-19 13:23:57 -0700

    all

--- 0x12-singly_linked_lists/100-main.c
+++ 0x12-singly_linked_lists/100-main.c
@@ -1,18 +1,12 @@
 #include <stdio.h>
 
 /**
- * print_mess - Apply the constructor attribute to myStartupFun() so that it
- * is executed before main()
- */
-void print_mess(void) __attribute__((constructor));
-
-/**
  * main - check the code
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+    printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
+    return (0);
 }
