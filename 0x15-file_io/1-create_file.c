999fda1fa65721045ff50fac9da920b1daa0ee1d
ossama21 <ossamahattan@gmail@com>
2023-10-30 14:52:39 -0700

    all

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,10 +10,16 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int file, flag;
+	int file, flag, len = 0;
 
 	if (filename == NULL)
 		return (-1);
+
+		if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
 
 	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	flag = write(file, text_content, strlen(text_content));
